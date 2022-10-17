fbd73c5e6718d42ef57da8a0eab4965db3a1707a
julia0bandeira <juliabandeira2004@gmail.com>
2022-10-17 18:31:04 -0300

    GameMania

--- Game Mania/js/script.js
+++ Game Mania/js/script.js
@@ -7,24 +7,16 @@ function cadastrarNewsletter() {
 const password = document.getElementById("password");
 const username = document.getElementById("username");
 
-document.getElementById("form").addEventListener('submit', (event) => {
+function logar(){
 
-    event.preventDefault()
-    valid = true
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
 
-    // Add your checks here :
-
-    const password_value = password.value;
-    const username_value = username.value;
-
-    if (username_value.length < 5 || password_value.length < 12) {
-        alert("The form has not been submitted : \n - A field is not valid.");
-        valid = false
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+        location.href = "index.html";
+    }else{
+        alert('Usuario ou senha incorretos');
     }
 
-    if (valid) alert("The form has been completed. \n We will proceed with the sending.");
-    // submit the form if everything is valid :
-    // if (valid) event.target.submit();
-
-})
-
+}
