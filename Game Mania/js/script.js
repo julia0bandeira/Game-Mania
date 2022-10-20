77c6b4b957040d6931101d8a73129b63b9f05635
julia0bandeira <juliabandeira2004@gmail.com>
2022-10-19 21:07:54 -0300

    GameMania

--- Game Mania/js/script.js
+++ Game Mania/js/script.js
@@ -1,3 +1,32 @@
+$(document).ready(function() {
+    
+    let slideAtual = 1;
+    let listaSlides = ["banner-joystick", "banner-headset", "banner-chair"]
+
+    setInterval(mudarSlide, 1500)
+
+    function mudarSlide() {
+        console.log("Slide atual:", slideAtual);
+
+        // Remove o slide anterior
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass(listaSlides[ (listaSlides.length) - 1 ])
+        }
+
+        // Adiciona o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual]);
+
+        // Indica o slide atual
+        slideAtual++
+
+        if(slideAtual > (listaSlides.length) - 1) {
+            slideAtual = 0;
+        }
+    }   
+})
+
 function cadastrarNewsletter() {
     let email = document.getElementById("campo-email").value
 
