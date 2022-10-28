06506468b7d08fd953f8b6dce36b263471010421
julia0bandeira <juliabandeira2004@gmail.com>
2022-10-28 14:14:41 -0300

    GameMania

--- Game Mania/js/script.js
+++ Game Mania/js/script.js
@@ -27,18 +27,23 @@ $(document).ready(function() {
     }   
 })
 
-// jquery ---------------------------------------------------
- $("#barras").click(function() {
+const btnMobile = document.getElementById('btn-mobile');
 
-     $("#menu").toggleClass("menu-ativo");
-    
-     Toggle
-     if( $("#menu").hasClass("menu-ativo") ) {
-         $("#menu").removeClass("menu-ativo")
-     } else {
-         $("#menu").addClass("menu-ativo")
-     }
- })
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
 
 function cadastrarNewsletter() {
     let email = document.getElementById("campo-email").value
